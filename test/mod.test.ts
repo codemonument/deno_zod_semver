4b49b803bc8a38616478230a092c665e9760d43a
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 12:07:32 +0200

    extend tests

--- test/mod.test.ts
+++ test/mod.test.ts
@@ -1,9 +1,17 @@
-import { assert, describe, it } from "std_testing";
+import { assert, assertThrows, describe, it } from "std_testing";
 import { regexSemverNumbered, ZodSemver } from "@/mod.ts";
 
 describe(`mod.ts`, () => {
   it(`should export correct objects and types`, () => {
     assert(regexSemverNumbered);
     assert(ZodSemver);
+  });
+
+  it(`should report 1.5.5 as valid semver`, () => {
+    ZodSemver.parse("1.5.5");
+  });
+
+  it(`should throw for input '1a.5.5' `, () => {
+    assertThrows(() => ZodSemver.parse("1a.5.5"));
   });
 });
