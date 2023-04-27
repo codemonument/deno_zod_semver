0c22009197f1ee785f037831b5cfbb2447881e53
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 11:53:27 +0200

    add disclaimer in deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,6 @@
-export * as log from "https://deno.land/std@0.184.0/log/mod.ts";
+/**
+ * These dependencies are used inside module code.
+ * DO NOT move them into import_map.json, since import_maps cannot be used when importing this module into other deno code!
+ */
 
+export * as log from "https://deno.land/std@0.184.0/log/mod.ts";
