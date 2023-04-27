bdafc98512acb0ab563f76a0a7d63b877550f2d9
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 12:51:04 +0200

    upgrade testing to std 182

--- test/utils/std_testing.ts
+++ test/utils/std_testing.ts
@@ -4,6 +4,6 @@
  */
 
 // eslint-disable-next-line deno-warn
-export * from "https://deno.land/std@0.180.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.180.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.180.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.182.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.182.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.182.0/testing/snapshot.ts";
