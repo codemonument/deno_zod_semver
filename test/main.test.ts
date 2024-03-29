097a27f97d1b81b14eb8f82f675f16d873adb62c
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-29 14:15:05 +0100

    fix tests to import src/main.ts

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,9 @@
 import { describe, it } from "@std/testing/bdd";
 import { assert, assertThrows } from "@std/assert";
-import { regexSemverNumbered, ZodSemver } from "@/mod.ts";
+import { regexSemverNumbered, ZodSemver } from "@/src/main.ts";
 import { z } from "zod";
 
-describe(`mod.ts`, () => {
+describe(`main.ts`, () => {
   it(`should export correct objects and types`, () => {
     assert(regexSemverNumbered);
     assert(ZodSemver);
